1b9216f4ec868839428c2280c384af50c246f780
Rustamscode <agagylrustamov@gmail.com>
2025-08-05 13:28:18 +0400

    feat-4(scheduler): add configuration for shedlock and redis

---
+++ src/main/java/rustamscode/order_ms/config/SchedulerConfig.java
@@ -0,0 +1,28 @@
+package rustamscode.order_ms.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+public class SchedulerConfig {
+
+  @Value("${outbox.scheduler.lock.environment}")
+  private String environment;
+
+  @Value("${outbox.scheduler.lock.keyPrefix}")
+  private String keyPrefix;
+
+  @Bean
+  public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
+    return new RedisLockProvider(connectionFactory, environment, keyPrefix);
+  }
+}
